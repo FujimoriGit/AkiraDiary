2dab6ab7a7b36a5d3b67f89bacd138c12eb3d224
Fujimori Daiki <G016C1102@outlook.jp>
2023-10-21 15:01:43 +0900

    #7 空アプリの作成

---
+++ Macho/Macho/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Macho
+//
+//  Created by 藤森大輝 on 2023/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
