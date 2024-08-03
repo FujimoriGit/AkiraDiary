4c559755c0c6c92caeb4c96a86de0cdf3e6eee35
佐藤汰一 <satoutaichi@satoutaichinoMacBook-Air.local>
2024-08-03 12:12:27 +0900

    Futureクラスの雛形作成

--- Macho/MachoFramework/Sources/MachoView/Views/DiaryList/Filter/DiaryListFilterFeature.swift
+++ Macho/MachoFramework/Sources/MachoView/Views/DiaryList/Filter/DiaryListFilterFeature.swift
@@ -5,8 +5,25 @@
 //  Created by 佐藤汰一 on 2024/07/28.
 //
 
-import Foundation
+import ComposableArchitecture
 
+@Reducer
 struct DiaryListFilterFeature {
     
+    struct State: Equatable, Sendable {
+        
+        
+    }
+    
+    enum Action {
+        
+        case onAppear
+    }
+    
+    var body: some ReducerOf<Self> {
+        
+        Reduce { state, action in
+            return .none
+        }
+    }
 }
