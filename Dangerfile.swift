9bfd04ef0c2ebb60650efdcad6620fe73a6131b4
佐藤汰一 <satoutaichi@satoutaichinoMacBook-Air.local>
2025-03-01 21:00:51 +0900

    PRの変更ファイル数でなく、変更行をレビューするように修正

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -9,11 +9,10 @@ if let github = danger.github {
         warn("PRの説明が短すぎます。100行以上は書いてね！")
     }
     
-    if (github.pullRequest.changedFiles ?? 0) >= 500 {
+    let changeLineCount = (github.pullRequest.additions ?? .zero) + (github.pullRequest.deletions ?? .zero)
+    if (changeLineCount) >= 500 {
         warn("PRの変更行が多すぎます。500行以内にしてね！理想は400行！")
     }
-    
-    message("PRの変更行は\(String(describing: danger.github.pullRequest.changedFiles))行です。")
 }
 
 // SwiftLintのレビュー
