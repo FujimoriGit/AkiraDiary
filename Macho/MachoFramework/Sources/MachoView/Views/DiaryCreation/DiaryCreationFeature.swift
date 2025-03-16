2d56fbe7f4a6fd258265bab207d4a6a747ecd4c2
stotic-dev <taichis844@gmail.com>
2025-03-16 09:44:54 +0900

    既存の日記を作成画面で編集できるように修正

--- Macho/MachoFramework/Sources/MachoView/Views/DiaryCreation/DiaryCreationFeature.swift
+++ Macho/MachoFramework/Sources/MachoView/Views/DiaryCreation/DiaryCreationFeature.swift
@@ -18,6 +18,8 @@ struct DiaryCreationFeature: Sendable {
     @ObservableState
     struct State: Equatable {
         
+        @ObservationStateIgnored var id: UUID?
+        var createdAt: Date?
         var titleText = ""
         var messageText = ""
         var tags: [Tag] = []
@@ -25,6 +27,7 @@ struct DiaryCreationFeature: Sendable {
         var isEnableStartButton = false
         var animationsRunning = false
         var textFieldFocusState: DiaryCreationTextFieldFocus?
+        var isEditMode = false
         
         @Presents var destination: Destination.State?
         @Presents var alert: AlertState<Action.Alert>?
@@ -40,6 +43,7 @@ struct DiaryCreationFeature: Sendable {
         case titleTextChange(String)
         case messageTextChange(String)
         case trainingStartButtonTapped
+        case trainingSaveButtonTapped
         case animateStartButton
         case destination(PresentationAction<Destination.Action>)
         case tappedAddingTagButton
@@ -105,6 +109,16 @@ struct DiaryCreationFeature: Sendable {
                     .run { [state] _ in
                         
                         await addDiary(state: state)
+                    },
+                    popToPrev()
+                )
+                
+            case .trainingSaveButtonTapped:
+                return .concatenate(
+                    .send(.animateStartButton),
+                    .run { [state] _ in
+                        
+                        await updateDiary(state: state)
                     },
                     popToPrev()
                 )
@@ -208,20 +222,30 @@ private extension DiaryCreationFeature {
         _ = await diaryListFetchApi.add(diary)
     }
     
+    func updateDiary(state: State) async {
+        
+        guard let id = state.id,
+              let createdAt = state.createdAt else {
+            
+            assertionFailure("Unexpected state.id is nil.")
+            return
+        }
+        let diary = DiaryData(id: id,
+                              date: createdAt,
+                              title: state.titleText,
+                              mainText: state.messageText,
+                              goals: state.goals.map(\.entity),
+                              tags: state.tags.map(\.entity),
+                              startTime: createdAt,
+                              endTime: nil)
+        _ = await diaryListFetchApi.add(diary)
+    }
+    
     func createDiary(state: State) -> DiaryData {
         
         let startDate = Date()
         
-        let goals = state.goals.map {
-            
-            TrainingContentData(id: $0.id,
-                                trainingType: $0.trainingType,
-                                goalNumberOfSets: $0.numberOfSets,
-                                goalSetCount: $0.setCount,
-                                actualNumberOfSets: nil,
-                                actualSetCount: nil)
-        }
-        
+        let goals = state.goals.map(\.entity)
         let tags = state.tags.map(\.entity)
         
         return DiaryData(id: UUID(),
@@ -304,6 +328,9 @@ extension DiaryCreationFeature.State {
     
     init(editTarget diary: DiaryData) {
         
+        isEditMode = true
+        id = diary.id
+        createdAt = diary.date
         titleText = diary.title
         messageText = diary.mainText
         tags = diary.tags.map { .init(entity: $0, isSelected: true) }
@@ -319,6 +346,5 @@ extension DiaryCreationFeature.State {
                          numberOfSets: $0.goalNumberOfSets,
                          setCount: $0.goalSetCount)
         }
-        
     }
 }
